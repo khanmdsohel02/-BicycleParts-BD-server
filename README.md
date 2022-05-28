2ad0d71ddb45d4624193a3a3fbc5cca919e9656b
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-05-28 22:12:22 +0600

    portfolio page done

---
+++ README.md
@@ -0,0 +1,18 @@
+## Project Name : bicyclepartsbd
+
+##  Live link : https://bicyclepartsbd-37611.web.app/
+
+# This website is basically bicycleparts manufacturer based website . Here various products of bicyclepart are manufacturer bicycleparts and supplied for sale to various retailers.
+
+## What has been done in the project 
+
+-  The website has a navigator and a dropdown menu has been added to the navbar. The navbar has to show the user profile or username
+
+- It uses tailwindcss, react, react hot toast, etc. to create a full stack website.
+
+-  Firebase authentication system used for authentication Email password , Google authentication systems have been implemented
+
+ - Various success messages and error messages have been shown using React Hot toast, Success message is also shown after adding the product
+
+
+- I couldn't apply many things nicely because I didn't know many things. I couldn't design enough because I had little idea of design.
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,80 +1,46 @@
 import React from 'react';
 
+
 const Blogs = () => {
     return (
-        <div className='mt-24'>
-           <Accordion className='m-5'>
-  <Accordion.Item eventKey="0">
-    <Accordion.Header>Difference between javascript and nodejs?</Accordion.Header>
-    <Accordion.Body>
-       JavaScript is a proper high - level programming language used to create web scripts whereas Node.js is a run time environment built on google’ s v8 engine.
-<br /><br />
-      JavaScript is executed in the browser whereas using Node.js gives us the ability to execute JavaScript outside the browser.
-<br /><br />
-      JavaScript can manipulate DOM or add HTML within whereas Node.js doesn’ t have the capability to add HTML.
-<br /><br />
-      JavaScript is mainly used to create front end web applications or develop client - side whereas Node.js is used on the back end development that is server - side development
-<br /><br />
-      JavaScript follows the standard of JavaScript when writing programs whereas Node.js is written in C++
-      while using the v8 engine, it runs JavaScript outside the browser.
-<br /><br />
-      JavaScript requires any running environment as it can execute on any engine such as Firefox’ s spider monkey, v8 engine of google chrome, JavaScript core of Safari whereas Node.js runs only on the v8 engine of google chrome.
-    </Accordion.Body>
-  </Accordion.Item>
-  <Accordion.Item eventKey="1">
-    <Accordion.Header>When should you use nodejs and when should you use mongodb?</Accordion.Header>
-                    <Accordion.Body>
-                        <strong>NodeJS</strong>
-                              <br /><br />
-     Node.js is one of the most popular environments, taking the lead
-     for the second consecutive year.While JavaScript is a programming language, Node.js is an ecosystem built on JS, leveraging the V8– Google server engine.V8 uses the JavaScript code to translate it into a faster machine code, readable by a computer.
-<br />
-     Before Node.js stepped in , JavaScript could only work in a browser
-     for writing the frontend part of web - apps.Yet, using JavaScript
-     for general - purpose applications was challenging.Rayan Dahl filled this gap in 2009 when he came up with a new environment to execute JavaScript separately from a web browser.
-<br />
-     Since then, JavaScript has been used to make much more than simple websites and interactive web apps.It expanded its use to microcontrollers, REST APIs, static file servers, and even Internet of Things(IoT) !
-                        <br /><br /><br />
-                        <strong>MongoDB</strong>
-                        <br /><br />
-MongoDB is basically an Open Source cross platform document oriented DB program, often classified as NoSQL DB program. What it means is that instead of the traditional table based RDBMS, the emphasis is on JSON with dynamic schemas.
- <br /><br />
-So when should this be used?
- <br /><br />
-High Write Load- If the need is to load rows of data without much security issues, you can be using it. However do avoid using with transactions, unless there is security implemented.
- <br /><br />
-High Availability in Cloud- It is easier to set up a series of master-slave servers and recovery from failure is faster too.
- <br /><br />
-Scalability- RDBMS has it’s own limitations when it comes to scalability, especially the performance which often tends to degrade. MongoDB comes with a built in solution for partition and database sharding.
- <br /><br />
-Big Data sets And Unstable Schema- When you are dealing with tables larger than 1 GB, whose schema is never consistent, MongoDB is a pretty good option. Whenever you add a new field in MongoDB, it does not effect existing rows, and is faster, unlike a traditional RBDMS.
-
-    </Accordion.Body>
-  </Accordion.Item>
-  <Accordion.Item eventKey="2">
-    <Accordion.Header>Differences between sql and nosql databases.?</Accordion.Header>
-    <Accordion.Body>
-     
-                  There are a lot of databases used today in the industry.Some are SQL databases, some are NoSQL databases.The conventional database is SQL database system that uses tabular relational model to represent data and their relationship.The NoSQL database is the newer one database that provides a mechanism
-                  for storage and retrieval of data other than tabular relations model used in relational databases.
-    </Accordion.Body>
-  </Accordion.Item>
-  <Accordion.Item eventKey="3">
-    <Accordion.Header>What is the purpose of jwt and how does it work?</Accordion.Header>
-    <Accordion.Body>
-     JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object.
-<br /><br />
-It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP). So the integrity and authenticity of the token can be verified by other parties involved.
-<br /><br />
-The purpose of using JWT is not to hide data but to ensure the authenticity of the data. JWT is signed and encoded, not encrypted.
-<br /><br />
-JWT is a token based stateless authentication mechanism. Since it is a client-side based stateless session, server doesn’t have to completely rely on a datastore(database) to save session information.
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-            
         
-        </div>
+        <div className='m-28'>
+            <div className='my-16'>
+                <h1 className=' text-2xl font-semibold uppercase'>How will you improve the performance of a React Application?</h1>
+                <p className='capitalize'>Keeping component state local where necessary.
+Memoizing React components to prevent unnecessary re-renders.
+Code-splitting in React using dynamic import()
+Windowing or list virtualization in React.
+Lazy loading images in React.optimization is the number one thing that is on the mind of every dev when building any software, especially web apps. JS frameworks like Angular, React and others, have included some awesome configurations and features. Here, I’ll review the features and tricks that will help you optimize your app’s performance.
+
+Regardless of the specific patterns and methods you use to optimize your code. It's always important to keep your code DRY. Always strive to reuse components — that’s guaranteed to help in writing optimized code. If you spend more time writing excellent code and less time re-writing mediocre code (with a much greater chance for errors) — wonderful things will happen. </p>
+            </div>
+            <div className='my-16'>
+                <h1 className=' text-2xl font-semibold uppercase'>What are the different ways to manage a state in a React application?</h1>
+                <p className='capitalize'>Managing state is arguably the hardest part of any application. It's why there are so many state management libraries available and more coming around every day (and even some built on top of others... There are hundreds of "easier redux" abstractions on npm). Despite the fact that state management is a hard problem, I would suggest that one of the things that makes it so difficult is that we often over-engineer our solution to the problem.
+
+There's one state management solution that I've personally tried to implement for as long as I've been using React, and with the release of React hooks (and massive improvements to React context) this method of state management has been drastically simplified.
+
+We often talk about React components as lego building blocks to build our applications, and I think that when people hear this, they somehow think this excludes the state aspect. The "secret" behind my personal solution to the state management problem is to think of how your application's state maps to the application's tree structure.</p>
+            </div>
+            <div className='my-16'>
+                <h1 className=' text-2xl font-semibold uppercase'> How does prototypical inheritance work?</h1>
+                <p className='capitalize'>JavaScript has only primitives types, null, undefined, and objects. A big world of objects. In JavaScript, contrary to languages like Java or PHP, there’s no concept of class that serves as a template to create objects.In JavaScript, an object can inherit properties of another object. The object from where the properties are inherited is named prototype.
+
+Following the example, you can make pet a prototype of cat which will then inherit legs property.
+
+When creating an object using the object literal, you can use the special property __proto__ to set the prototype of the created object.</p>
+            </div>
+           
+            <div className='my-16'>
+                <h1 className=' text-2xl font-semibold uppercase'>What is a unit test? Why should write unit tests?</h1>
+                <p className='capitalize'>Unit testing is a way to test units - the smallest components of your software, the smallest piece of code. A unit can be a single function. The goal is to validate that each unit performs as it should. A unit test tests a behavior in isolation to other tests. If the test relies on an external system, it is not a Unit Test. Unit Tests should be written during the design phase, prior to implementation to prevent defects from being deployed to production. They should be run every time the code is changed and provide a clear description of the feature being tested.
+
+Unit Test coverage is a subtype of Code Coverage - it shows which lines of code were tested by Unit Tests. However, the goal is not to have a 100% Unit Test coverage. In fact, you should have as few Unit Tests as possible and cover as many types of behavior as possible. 100% Unit Test coverage exists only in a perfect world. While writing Unit Tests focus on behaviors - adding new tests just to improve coverage leads to tests without any purpose and you would be just wasting your time.
+
+Unit Testing can be done manually but it is usually automated. In terms of automated tests, there is a concept called the test pyramid that shows how to efficiently balance the automated tests.</p>
+            </div>
+       </div>
     );
 };
 
